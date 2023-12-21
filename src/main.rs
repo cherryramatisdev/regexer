5461b0135bae9310b5c84aa0e9a7f1616ecb253d
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-12-21 15:08:56 -0300

    chore: revert main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@
 mod tokenizer;
 
 fn main() {
-    let a = "group(letter(upcase) | glob(rest=True) | whitespace) | number".to_string();
-    println!("{:?}", tokenizer::tokens::parse(a));
+    println!("Hello World");
 }

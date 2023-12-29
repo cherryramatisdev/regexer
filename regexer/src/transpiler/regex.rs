c6eee2a99d41f9e92e6c4942063b18ecfad64010
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-12-29 13:45:11 -0300

    feat: add transpilation for the select field

--- regexer/src/transpiler/regex.rs
+++ regexer/src/transpiler/regex.rs
@@ -3,19 +3,28 @@ use crate::lexer::ast::{Casing, Function};
 pub fn transpile(functions: Vec<Function>) -> String {
     return functions.iter().fold(String::from(""), |mut acc, func| {
         match func {
-            Function::Letter { casing } => match casing {
-                Casing::Upcase => {
-                    acc.push_str("[A-Z]");
+            Function::Letter { casing, select } => {
+                match casing {
+                    Some(Casing::Upcase) => {
+                        acc.push_str("[A-Z]");
+                    }
+                    Some(Casing::Downcase) | None => {
+                        acc.push_str("[a-z]");
+                    }
                 }
-                Casing::Downcase => {
-                    acc.push_str("[a-z]");
+
+                match select {
+                    Some(num) => {
+                        acc.push_str(format!("{{{num}}}").as_str());
+                    }
+                    None => {}
                 }
-            },
+            }
             Function::Letters { casing } => match casing {
-                Casing::Upcase => {
+                Some(Casing::Upcase) => {
                     acc.push_str("[A-Z]+");
                 }
-                Casing::Downcase => {
+                Some(Casing::Downcase) | None => {
                     acc.push_str("[a-z]+");
                 }
             },
@@ -30,8 +39,15 @@ pub fn transpile(functions: Vec<Function>) -> String {
             Function::Whitespace => {
                 acc.push_str(r"\s");
             }
-            Function::Number => {
+            Function::Number { select } => {
                 acc.push_str("[0-9]");
+
+                match select {
+                    Some(num) => {
+                        acc.push_str(format!("{{{num}}}").as_str());
+                    }
+                    None => {}
+                }
             }
             Function::Numbers => {
                 acc.push_str("[0-9]+");
